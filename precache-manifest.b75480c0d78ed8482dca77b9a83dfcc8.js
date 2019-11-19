b29ae465e6b25869fbed3b8f8fd295c521a6e56c
youvovk <yuriy3vovk@gmail.com>
2019-11-19 17:16:30 +0200

    Updates

---
+++ precache-manifest.b75480c0d78ed8482dca77b9a83dfcc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61bb6e0a2d67cf9c4f2d70bde41b9985",
+    "url": "/casino4/index.html"
+  },
+  {
+    "revision": "f93024f7533ec9833b55",
+    "url": "/casino4/static/css/main.5c5fd612.chunk.css"
+  },
+  {
+    "revision": "dd9018f28c91046c77ab",
+    "url": "/casino4/static/js/2.d5a5c860.chunk.js"
+  },
+  {
+    "revision": "f93024f7533ec9833b55",
+    "url": "/casino4/static/js/main.e452ca36.chunk.js"
+  },
+  {
+    "revision": "8e4a0271521405e02053",
+    "url": "/casino4/static/js/runtime-main.1f0db6f8.js"
+  },
+  {
+    "revision": "37929eedcc16b8491692d89badc6c51a",
+    "url": "/casino4/static/media/back-main.37929eed.jpg"
+  }
+]);
